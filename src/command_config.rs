f3a6bffa748ae701a47ffcd18105d66c9a3e758c
Simeon David Schaub <schaub@mit.edu>
2021-12-02 19:55:51 -0500

    only modify all symlinks if value changed

--- src/command_config.rs
+++ src/command_config.rs
@@ -6,26 +6,33 @@ pub fn run_command_config(property: String, value: String) -> Result<()> {
     let mut config_data =
         load_config_db().with_context(|| "`config` command failed to load configuration file.")?;
 
+    let mut value_changed = false;
+
     match property.as_str() {
         "symlinks" => {
             if std::env::consts::OS == "windows" {
                 bail!("Symlinks not supported on Windows.");
             }
 
-            config_data.create_symlinks = match value.as_str() {
+            let create_symlinks = match value.as_str() {
                 "on"  => true,
                 "off" => false,
                 _     => bail!("Value for 'symlinks' must be either 'on' or 'off'."),
             };
 
-            if config_data.create_symlinks {
-                for (channel_name, channel) in &config_data.installed_channels {
-                    create_symlink(channel, &format!("julia-{}", channel_name))?;
+            if create_symlinks != config_data.create_symlinks {
+                config_data.create_symlinks = create_symlinks;
+                value_changed = true;
+
+                if create_symlinks {
+                    for (channel_name, channel) in &config_data.installed_channels {
+                        create_symlink(channel, &format!("julia-{}", channel_name))?;
+                    }
                 }
-            }
-            else {
-                for (channel_name, _) in &config_data.installed_channels {
-                    remove_symlink(&format!("julia-{}", channel_name))?;
+                else {
+                    for (channel_name, _) in &config_data.installed_channels {
+                        remove_symlink(&format!("julia-{}", channel_name))?;
+                    }
                 }
             }
         },
@@ -34,7 +41,12 @@ pub fn run_command_config(property: String, value: String) -> Result<()> {
 
     save_config_db(&config_data)?;
 
-    eprintln!("Property '{}' set to '{}'", property, value);
+    if value_changed {
+        eprintln!("Property '{}' set to '{}'", property, value);
+    }
+    else {
+        eprintln!("Property '{}' is already set to '{}'", property, value);
+    }
 
     Ok(())
 }
